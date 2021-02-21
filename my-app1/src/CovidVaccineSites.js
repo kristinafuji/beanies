64820c98686adce6cd133ce9f22836dc990ab601
kristinafuji <79348269+kristinafuji@users.noreply.github.com>
2021-02-21 04:36:37 -0500

    boo

--- my-app1/src/CovidVaccineSites.js
+++ my-app1/src/CovidVaccineSites.js
@@ -9,28 +9,37 @@ export function CovidVaccineSites() {
     const [zipCode, setZipCode] = useState(['']);
     //this.state={zipCode: ''};
     const [zipCodeData, setZipCodeData] = useState([]);
-  useEffect(() => {
-    let mounted = true;
-    getList1()
-      .then(items => {
-        if(mounted) {
-            setZipCodeData(items)
-          console.log(items)
-        }
-      })
-    return () => mounted = false;
-  }, [])
-  return (
-      <div>
-      <div>
-        <input onChange={event => setZipCode(event.target.value)}/>
-        <Button style={{backgroundColor: "#9bb1db", color:"white"}} variant="outline-*" onClick={() => {getList1(zipCode)}}>Submit</Button>{' '}
-      </div>
-      {/* <div>
-          if (zipCodeData != undefined) {
-              zipCideData.map(entry => entry);
-          }
-      </div> */}
-      </div>
-      )
-    }
+    useEffect(() => {
+        let mounted = true;
+        // getList1()
+        //   .then(items => {
+        //     if(mounted) {
+        //         setZipCodeData(items)
+        //       console.log(items)
+        //     }
+        //   })
+        return () => mounted = false;
+    }, [])
+    return (
+        <div>
+            <div>
+                <input onChange={event => setZipCode(event.target.value)} />
+                <Button style={{ backgroundColor: "#9bb1db", color: "white" }} variant="outline-*" onClick={() => {
+                    getList1(zipCode).then(items => {
+                        setZipCodeData(items)
+                    })
+                }}>Submit</Button>{' '}
+            </div>
+            {zipCodeData?.map(entry => <Address {...entry}/>)}
+        </div>
+    )
+}
+
+function Address(object) {
+    return <div class="addressI">
+        <p>{object.name}</p>
+        <p>address: {object.address}</p>
+        <p>hours: {object.hours}</p>
+        <p>serves: {object.serves}</p>
+    </div>
+}
--- my-app1/src/services/covidTestService.js
+++ my-app1/src/services/covidTestService.js
@@ -2,5 +2,4 @@ export function getList1(zipCode) {
     console.log(typeof zipCode)
     return fetch(`http://localhost:8181/locations?zipcode=${zipCode}`)
     .then(data => data.json())
-    .then(data => console.log(data))
 }
